a6e5a87826ce5d8e8db00ee80f00d904b57fecb0
Vung Nguyen <vungnguyenn1001@gmail.com>
2023-11-07 02:55:23 +0700

    categories

---
+++ src/components/categories.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+
+
+
+export default function Categories({ categories, activeCategory, handleChangeCategory }) {
+    return (
+        <View className="mx-4 space-y-4">
+            <Text style={{ fontSize: hp(3), fontFamily: 'Inter-Bold' }} className=" text-black">Categories</Text>
+            <View>
+                <Animated.View entering={FadeInDown.duration(500).springify()}>
+                    <ScrollView
+                        horizontal
+                        className="space-x-4"
+                        showsHorizontalScrollIndicator={false}
+                        contentContainerStyle={{ paddingHorizontal: 15 }}
+                    >
+                        {
+                            categories.map((cate, index) => {
+                                let isActive = cate.id == activeCategory
+                                let activeButtonClass = isActive ? ' bg-amber-400' : ' bg-black/10'
+                                return (
+                                    <TouchableOpacity
+                                        key={index}
+                                        onPress={() => handleChangeCategory(cate.id)}
+                                        className="flex items-center space-y-1"
+                                    >
+                                        <View className={"rounded-full p-[5px]" + activeButtonClass}>
+                                            <Image
+                                                source={{ uri: cate.img }}
+                                                style={{ width: hp(6.5), height: hp(6.5) }}
+                                                className="rounded-full" />
+                                        </View>
+                                        <Text className="text-neutral-600 font-mono" style={{ fontSize: hp(1.6), fontFamily: 'Inter-Medium' }}>{cate.name}</Text>
+                                    </TouchableOpacity>
+                                )
+                            })
+                        }
+                    </ScrollView>
+                </Animated.View>
+            </View>
+        </View>
+
+
+    )
+}
---
+++ src/components/form/AddCategory.js
@@ -0,0 +1,167 @@
+import { View, Text, Modal, TouchableOpacity, TextInput, StatusBar, Image, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import Animated, { FadeIn, FadeInDown, FadeInUp } from 'react-native-reanimated';
+import ImagePicker from 'react-native-image-crop-picker';
+import uuid from 'react-native-uuid';
+import { getDownloadURL, ref as storageRef, uploadBytes, uploadBytesResumable } from "firebase/storage";
+import { storage, database, auth } from '../config/FirebaseConfig';
+
+export default function AddCategory({ visible, onClose, onSubmit }) {
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [idCate, setIdCate] = useState(uuid.v4())
+    const [nameCate, setNameCate] = useState("")
+    const [loading, isLoading] = useState(false)
+    const [progress, setProgress] = useState(0)
+
+    const clearForm = () => {
+        setNameCate("")
+        setSelectedImage(null)
+    }
+    const handleClose = () => {
+        clearForm()
+        onClose();
+    };
+
+
+    const handleSubmit = async () => {
+        if (nameCate.length == 0 || nameCate == null) {
+            alert("Nhập tên thể loại")
+            return;
+        } else if (selectedImage == null) {
+            alert("Vui lòng chọn ảnh")
+            return;
+        }
+        onSubmit(idCate, nameCate, selectedImage);
+        clearForm()
+    };
+
+
+    const customLogger = (message) => {
+        console.log(message);
+    }
+
+    const handleImagePicker = () => {
+        ImagePicker.openPicker({
+            maxHeight: 512,
+            maxWidth: 512,
+            cropping: true,
+        }).then(async (image) => {
+            const selectedImage = { uri: image.path };
+            setSelectedImage(selectedImage)
+            // await uploadImageToFirebase(selectedImage, "png");
+        }).catch(error => {
+            if (error.message !== "User cancelled image selection") {
+                customLogger(error.message);
+            }
+        });
+    };
+
+
+    return (
+        <Modal visible={visible} animationType="fade" transparent={true}>
+            <View style={styles.container}>
+                <View style={styles.dialog}>
+                    <Text style={{ fontFamily: 'Inter-Medium' }} className="text-black text-center text-2xl py-2">Thêm mới thể loại</Text>
+
+                    <Text style={{ fontFamily: 'Inter-Medium' }} className="text-black py-1 hidden">Mã thể loại</Text>
+                    <TextInput
+                        className="text-black p-3 rounded border-gray-300 border hidden"
+                        style={{ fontFamily: 'Inter-Medium' }}
+                        value={idCate}
+                        editable={false}
+                        focusable={false}
+                    />
+
+                    <Text style={{ fontFamily: 'Inter-Medium' }} className="text-black py-1">Tên thể loại</Text>
+                    <TextInput
+                        className="text-black p-3 rounded border-gray-300 border"
+                        style={{ fontFamily: 'Inter-Medium' }}
+                        value={nameCate}
+                        onChangeText={(text) => setNameCate(text)}
+                    />
+
+                    {/* Rest of the component */}
+                    {selectedImage && <Image
+                        className="w-36 h-36 my-3"
+                        source={{ uri: `${selectedImage.uri}` }} />}
+                    <Animated.View
+                        entering={FadeInDown.delay(400).duration(1000).springify()}>
+                        <TouchableOpacity
+                            className=" bg-sky-400  py-2 px-2 rounded-lg mb-3 my-2"
+                            onPress={handleImagePicker}>
+                            <Text style={{ fontFamily: 'Inter-Medium' }} className="text-sm  text-white text-center">Select Image</Text>
+                        </TouchableOpacity>
+
+                    </Animated.View>
+                    {/* Rest of the component */}
+                    <View className="flex-row justify-around mt-3">
+                        <Animated.View
+                            entering={FadeInDown.delay(400).duration(1000).springify()}>
+                            {loading ? <ActivityIndicator size='large' color='#38bdf8' /> : <>
+                                <TouchableOpacity
+                                    className=" bg-sky-400  py-2 px-3 rounded-lg mb-3 "
+                                    onPress={handleSubmit}>
+                                    <Text style={{ fontFamily: 'Inter-Medium' }} className="text-sm  text-white text-center">Thêm mới</Text>
+                                </TouchableOpacity>
+                            </>}
+                        </Animated.View>
+                        <Animated.View
+                            entering={FadeInDown.delay(400).duration(1000).springify()}>
+
+                            <TouchableOpacity
+                                className=" bg-gray-400 py-2 px-4 rounded-lg mb-3"
+                                onPress={handleClose}>
+                                <Text style={{ fontFamily: 'Inter-Medium' }} className="text-sm  text-white text-center">Cancel</Text>
+                            </TouchableOpacity>
+
+                        </Animated.View>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    dialog: {
+        backgroundColor: 'white',
+        padding: 16,
+        borderRadius: 8,
+        elevation: 4,
+        width: '80%',
+    },
+    input: {
+        marginBottom: 12,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    button: {
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        borderRadius: 4,
+        backgroundColor: '#007BFF',
+        marginBottom: 12,
+        width: '48%',
+    },
+    buttonCancel: {
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        borderRadius: 4,
+        backgroundColor: '#dbc6c6',
+        marginBottom: 12,
+        width: '48%',
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
